0c877699b28c596f0bb4c9c756be0bfbe9bada64
Mac71805 <miguel71805@gmail.com>
2026-04-17 14:37:51 -0400

    Finish the properties, constructor, method

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,4 +1,66 @@
 package org.example;
 
 public class Book {
-}
+    private String id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public Book(String id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+        public void checkOut (String name){
+            isCheckedOut = true;
+            checkedOutTo = name;
+        }
+
+        public void checkIn () {
+            isCheckedOut = false;
+            checkedOutTo = "";
+        }
+    }
